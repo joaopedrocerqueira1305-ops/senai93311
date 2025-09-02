f55c2c856bf0f61efa42cb7daaf45066efd57d2d
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-02 08:32:51 -0300

    teste

---
+++ Heranca/src/main/java/org/example/polimorfismoAtiv/Animal.java
@@ -0,0 +1,4 @@
+package org.example.polimorfismoAtiv;
+
+public interface Animal {
+}
---
+++ Heranca/src/main/java/org/example/polimorfismoAtiv/Main.java
@@ -0,0 +1,7 @@
+package org.example.polimorfismoAtiv;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("tetrfgvsdheethrhr");
+    }
+}
